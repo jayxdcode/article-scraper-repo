8b4ed9943450e6d59bbcea6b21ffe868f583780e
jl14_ <118164738+github-jl14@users.noreply.github.com>
2024-10-23 02:13:43 +0800

    Update scrape_inquirer.py
    fixed SyntaxError

--- scrape_inquirer.py
+++ scrape_inquirer.py
@@ -55,9 +55,8 @@ def extract_inquirer_content(article_url):
                 paragraph_text = tag.get_text()
                 # Filter out unwanted paragraphs
                 if "Subscribe to our daily newsletter" in paragraph_text or \
-                   "Subscribe to our newsletter!" in paragraph_text:
-                  or \
-                   "By providing an email address. I agree to the Terms of Use and acknowledge that I have read the Privacy Policy."
+                   "Subscribe to our newsletter!" in paragraph_text or \
+                   "By providing an email address. I agree to the Terms of Use and acknowledge that I have read the Privacy Policy." in paragraph_text:
                     continue
                 article_content.append(paragraph_text)
 
