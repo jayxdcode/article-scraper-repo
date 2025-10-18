23815a498a20b4b2f6f9ebb3403e7c525ab82ab4
J.L. De Guzman <118164738+jayxdcode@users.noreply.github.com>
2025-10-18 11:59:44 +0800

    Update scrape-inquirer.js

--- node/inquirer/scrape-inquirer.js
+++ node/inquirer/scrape-inquirer.js
@@ -95,7 +95,8 @@ async function run() {
   const browser = await puppeteer.launch({
     headless: config.puppeteer?.headless !== false,
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    defaultViewport: { width: 1200, height: 900 }
+    defaultViewport: { width: 1200, height: 900 },
+    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || undefined
   });
   const page = await browser.newPage();
   await page.setUserAgent(new UserAgent().toString());
